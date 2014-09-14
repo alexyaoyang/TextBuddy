c3a5f7391cc925a839a04e148617e1f7a0185753
Alex Yao <alexyaoyang@hotmail.com>
2014-09-14 16:21:26 +0800

    reindent

--- TextBuddy/TextBuddy.cpp
+++ TextBuddy/TextBuddy.cpp
@@ -19,11 +19,11 @@ using namespace std;
 ofstream writeFile;
 ifstream readFile;
 string fileName;
-const char delim = '^';
-const string defaultFileName = "mytextfile.txt";
-const string unableToOpenFile = "Unable to open file";
-const string paramsError = "Please specify correct parameters!";
-const string add = "add";
+const char DELIM = '^';
+const string DEFAULT_FILE_NAME = "mytextfile.txt";
+const string MESSAGE_UNABLE_TO_OPEN_FILE = "Unable to open file";
+const string MESSAGE_PARAM_ERROR = "Please specify correct parameters!";
+const string ADD = "add";
 const string display = "display";
 const string del = "delete";
 const string clear = "clear";
@@ -49,76 +49,76 @@ string getCommandParams(string cmd);
 string trim(string& str);
 
 int main(int argc, char* argv[]){
-	getFileNameFromArgument(argv);
-	makeFile();
-	printWelcome();
-	listenForCommands();
-
-	return 0;
+    getFileNameFromArgument(argv);
+    makeFile();
+    printWelcome();
+    listenForCommands();
+    
+    return 0;
 }
 
 void getFileNameFromArgument(char* argv[]){
-	if (!argv[1]){
-		printMsg("Argument not found, defaulting file name to " + defaultFileName);
-		fileName = defaultFileName;
-	}
-	else {
-		fileName = argv[1];
+    if (!argv[1]){
+        printMsg("Argument not found, defaulting file name to " + DEFAULT_FILE_NAME);
+        fileName = DEFAULT_FILE_NAME;
+    }
+    else {
+        fileName = argv[1];
         if(fileName.find(".txt")== string::npos){
             fileName = fileName + ".txt";
         }
-	}
+    }
 }
 
 void makeFile(){
-	writeFile.open(fileName);
+    writeFile.open(fileName);
 }
 
 void printWelcome(){
-	printMsg("Welcome to TextBuddy. " + fileName + " is ready for use");
+    printMsg("Welcome to TextBuddy. " + fileName + " is ready for use");
 }
 
 string getInputString(){
-	string cmd;
-	getline(cin, cmd);
-	return cmd;
+    string cmd;
+    getline(cin, cmd);
+    return cmd;
 }
 
 void printMsg(string msg){
-	cout << msg << endl;
+    cout << msg << endl;
 }
 
 void delegateTaskWithCommand(string cmd){
-	closeFiles(); //prepare for new operations
-
-	if (searchKeyInString(cmd, add)){
-		if (getCommandParams(cmd).empty()){
-			printMsg(paramsError);
-		}
-		else {
-			writeToFile(getCommandParams(cmd), append);
-		}
-	}
-	else if (searchKeyInString(cmd, del)){
-		if (getCommandParams(cmd).empty()){
-			printMsg(paramsError);
-		}
-		else {
-			deleteFromFile(getCommandParams(cmd));
-		}
-	}
-	else if (searchKeyInString(cmd, display)){
-		displayFromFile();
-	}
-	else if (searchKeyInString(cmd, clear)){
-		clearFileContents();
-	}
-	else if (searchKeyInString(cmd, quit)){
-		exit(0);
-	}
-	else {
-		printMsg("Invalid command, please re-enter.");
-	}
+    closeFiles(); //prepare for new operations
+    
+    if (searchKeyInString(cmd, ADD)){
+        if (getCommandParams(cmd).empty()){
+            printMsg(MESSAGE_PARAM_ERROR);
+        }
+        else {
+            writeToFile(getCommandParams(cmd), append);
+        }
+    }
+    else if (searchKeyInString(cmd, del)){
+        if (getCommandParams(cmd).empty()){
+            printMsg(MESSAGE_PARAM_ERROR);
+        }
+        else {
+            deleteFromFile(getCommandParams(cmd));
+        }
+    }
+    else if (searchKeyInString(cmd, display)){
+        displayFromFile();
+    }
+    else if (searchKeyInString(cmd, clear)){
+        clearFileContents();
+    }
+    else if (searchKeyInString(cmd, quit)){
+        exit(0);
+    }
+    else {
+        printMsg("Invalid command, please re-enter.");
+    }
 }
 
 void listenForCommands(){
@@ -129,18 +129,18 @@ void listenForCommands(){
 }
 
 bool searchKeyInString(string& cmd, string key){
-	return cmd.find(key) == 0;
+    return cmd.find(key) == 0;
 }
 
 string getCommandParams(string cmd){
-	int found = cmd.find(' ');
-	if (found == string::npos){
-		return "";
-	}
-	else {
-		cmd = cmd.substr(found + 1);
+    int found = cmd.find(' ');
+    if (found == string::npos){
+        return "";
+    }
+    else {
+        cmd = cmd.substr(found + 1);
         return trim(cmd);
-	}
+    }
 }
 
 string trim(string& str){
@@ -153,94 +153,94 @@ string trim(string& str){
 }
 
 void closeFiles(){
-	readFile.close();
-	writeFile.close();
+    readFile.close();
+    writeFile.close();
 }
 
 void writeToFile(string toStore, int mode){
-	if (mode == append){
-		writeFile.open(fileName, ios::app);
-	}
-	else if (mode == overwrite){
-		writeFile.open(fileName);
-	}
-	if (writeFile.good()){
-		writeFile << toStore;
-		if (mode == 1){
-			writeFile << delim;
-			printMsg("added to " + fileName + ": \"" + toStore + "\"");
-		}
-	}
-	else {
-		printMsg(unableToOpenFile);
-	}
+    if (mode == append){
+        writeFile.open(fileName, ios::app);
+    }
+    else if (mode == overwrite){
+        writeFile.open(fileName);
+    }
+    if (writeFile.good()){
+        writeFile << toStore;
+        if (mode == 1){
+            writeFile << DELIM;
+            printMsg("added to " + fileName + ": \"" + toStore + "\"");
+        }
+    }
+    else {
+        printMsg(MESSAGE_UNABLE_TO_OPEN_FILE);
+    }
 }
 
 void deleteFromFile(string l){
-	readFile.open(fileName);
-	if (isEmptyFile()){
-		printMsg(fileName + " is empty");
-	}
-	else if (readFile.good()){
-		int line = stoi(l);
+    readFile.open(fileName);
+    if (isEmptyFile()){
+        printMsg(fileName + " is empty");
+    }
+    else if (readFile.good()){
+        int line = stoi(l);
         if(line <= 0){
-            printMsg(paramsError);
+            printMsg(MESSAGE_PARAM_ERROR);
             return;
         }
-		int start = 0;
-		int end = 0;
-		string value;
-		getline(readFile, value);
-		for (int i = 0; i < line; i++){
-			start = end;
-			end = value.find(delim, start) + 1;
-		}
+        int start = 0;
+        int end = 0;
+        string value;
+        getline(readFile, value);
+        for (int i = 0; i < line; i++){
+            start = end;
+            end = value.find(DELIM, start) + 1;
+        }
         string toDel = value.substr(start, end - start - 1);
         if(toDel.empty()){
-            printMsg(paramsError);
+            printMsg(MESSAGE_PARAM_ERROR);
             return;
         }
-		printMsg("deleted from " + fileName + ": " + toDel);
-		value.erase(start, end - start);
-		readFile.close();
-		writeToFile(value, overwrite);
-	}
-	else {
-		printMsg(unableToOpenFile);
-	}
+        printMsg("deleted from " + fileName + ": " + toDel);
+        value.erase(start, end - start);
+        readFile.close();
+        writeToFile(value, overwrite);
+    }
+    else {
+        printMsg(MESSAGE_UNABLE_TO_OPEN_FILE);
+    }
 }
 
 void displayFromFile() {
-	readFile.open(fileName);
-	if (isEmptyFile()){
-		printMsg(fileName + " is empty");
-	}
-	else if (readFile.good()){
-		string line;
-		int i = 1;
-		while (!readFile.eof()){
-			getline(readFile, line, delim);
-			if (line != ""){
+    readFile.open(fileName);
+    if (isEmptyFile()){
+        printMsg(fileName + " is empty");
+    }
+    else if (readFile.good()){
+        string line;
+        int i = 1;
+        while (!readFile.eof()){
+            getline(readFile, line, DELIM);
+            if (line != ""){
                 cout<<i;
-				printMsg(". " + line);
-				i++;
-			}
-		}
-	}
-	else {
-		printMsg(unableToOpenFile);
-	}
+                printMsg(". " + line);
+                i++;
+            }
+        }
+    }
+    else {
+        printMsg(MESSAGE_UNABLE_TO_OPEN_FILE);
+    }
 }
 
 bool isEmptyFile(){
-	readFile.seekg(0, ios::end);
-	int length = (int)readFile.tellg();
-	readFile.seekg(0, ios::beg);
-	return length == 0;
+    readFile.seekg(0, ios::end);
+    int length = (int)readFile.tellg();
+    readFile.seekg(0, ios::beg);
+    return length == 0;
 }
 
 void clearFileContents(){
-	remove(fileName.c_str());
-	printMsg("all content deleted from " + fileName);
-	writeFile.open(fileName);
+    remove(fileName.c_str());
+    printMsg("all content deleted from " + fileName);
+    writeFile.open(fileName);
 }
